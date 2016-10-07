3119c10e1083f0e380bd48c5a9bfb9ad24b514e0
Daniel Cousens <github@dcousens.com>
2016-10-08 00:03:46 +1100

    fastroot: clearer variable names

--- fastRoot.js
+++ fastRoot.js
@@ -4,22 +4,21 @@ module.exports = function fastRoot (values, digestFn) {
   if (typeof digestFn !== 'function') throw TypeError('Expected digest Function')
 
   var length = values.length
-  var result = values.concat()
+  var results = values.concat()
 
   while (length > 1) {
     var j = 0
 
     for (var i = 0; i < length; i += 2, ++j) {
-      var input = Buffer.concat([
-        result[i],
-        i + 1 !== length ? result[i + 1] : result[i]
-      ])
+      var left = results[i]
+      var right = i + 1 === length ? left : results[i + 1]
+      var data = Buffer.concat([left, right])
 
-      result[j] = digestFn(input)
+      results[j] = digestFn(data)
     }
 
     length = j
   }
 
-  return result[0]
+  return results[0]
 }
